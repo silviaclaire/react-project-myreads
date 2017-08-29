c013e90623bf74995cdba1c7900249e234cae364
Claire Yan <silviaclaire@gmail.com>
2017-08-29 18:50:35 +0900

    Add ShowBooks component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import ShowBooks from './ShowBooks'
 
 const bookshelves = [
   { 'name': 'currentlyReading',
@@ -34,36 +35,10 @@ class ListBooks extends React.Component {
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">{bookshelf.title}</h2>
                   <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {books
-                        .filter(book => book.shelf === bookshelf.name)
-                        .map(book => (
-                          <li key={book.id}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{
-                                  width: 128, height: 193,
-                                  backgroundImage: `url(${book.imageLinks.thumbnail})`
-                                }}/>
-                                <div className="book-shelf-changer">
-                                  <select
-                                    onChange={(e) => onUpdateShelf(book, e.target.value)}
-                                    defaultValue={book.shelf}>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
-                            </div>
-                          </li>
-                        ))
-                      }
-                    </ol>
+                    <ShowBooks
+                      books={books.filter(book => book.shelf === bookshelf.name)}
+                      onUpdateShelf={onUpdateShelf}
+                    />
                   </div>
                 </div>
               </div>
---
+++ src/ShowBooks.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class ShowBooks extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onUpdateShelf: PropTypes.func.isRequired
+  }
+
+  render() {
+    const { books, onUpdateShelf } = this.props
+    return (
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{
+                  width: 128, height: 193,
+                  backgroundImage: `url(${book.imageLinks.thumbnail})`
+                }}/>
+                <div className="book-shelf-changer">
+                  <select
+                    onChange={(e) => onUpdateShelf(book, e.target.value)}
+                    defaultValue={book.shelf}>
+                    <option value="none" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    )
+  }
+}
+
+export default ShowBooks
