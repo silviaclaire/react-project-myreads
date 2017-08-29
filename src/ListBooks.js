657823c19cdef46cf347178ef05af1cf3e57bd7c
Claire Yan <silviaclaire@gmail.com>
2017-08-29 18:02:17 +0900

    Add books according to shelves

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -20,7 +20,6 @@ class ListBooks extends React.Component {
   }
 
   render() {
-    console.log('Props', this.props)
     return (
       <div>
         <div className="list-books">
@@ -34,7 +33,32 @@ class ListBooks extends React.Component {
                   <h2 className="bookshelf-title">{bookshelf.title}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <p>Books!</p>
+                      {this.props.books
+                        .filter(book => book.shelf === bookshelf.name)
+                        .map(book =>
+                          <li key={book.id}>
+                            <div className="book">
+                              <div className="book-top">
+                                <div className="book-cover" style={{
+                                  width: 128, height: 193,
+                                  backgroundImage: `url(${book.imageLinks.thumbnail})`
+                                }}/>
+                                <div className="book-shelf-changer">
+                                  <select defaultValue={book.shelf}>
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{book.title}</div>
+                              <div className="book-authors">{book.authors}</div>
+                            </div>
+                          </li>
+                        )
+                      }
                     </ol>
                   </div>
                 </div>
