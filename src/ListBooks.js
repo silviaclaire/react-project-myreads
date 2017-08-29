4163d9bf454203a269ab47f7f1b1391b6f2a31fd
Claire Yan <silviaclaire@gmail.com>
2017-08-29 17:58:21 +0900

    Add bookshelf titles

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,6 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
+const bookshelves = [
+  { 'name': 'currentlyReading',
+    'title': 'Currently Reading'
+  },
+  { 'name': 'wantToRead',
+    'title': 'Want to Read'
+  },
+  { 'name': 'read',
+    'title': 'Read'
+  }
+]
+
 class ListBooks extends React.Component {
   static propTypes = {
     books: PropTypes.array.isRequired
@@ -16,7 +28,18 @@ class ListBooks extends React.Component {
             <h1>MyReads</h1>
           </div>
           <div className="list-books-content">
-            <span>List Books!</span>
+            {bookshelves.map((bookshelf) => (
+              <div key={bookshelf.name}>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{bookshelf.title}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <p>Books!</p>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
           <div className="open-search">
             <Link to='/search'>Add a book</Link>
